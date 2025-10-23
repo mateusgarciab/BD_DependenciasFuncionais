d6e29606329f9ee40dfb76d54d9d4fd9b5c459ce
Mateus Garcia <101533021+mateusgarciab@users.noreply.github.com>
2025-10-23 16:57:58 +0000

    so anotações

--- leArquivo.c
+++ leArquivo.c
@@ -13,13 +13,22 @@ FILE *devolveArquivo(char *nome) {
     return arq;
 }
 
+char *devolveAtributos(FILE *arq) {
+    char *atributos = malloc(sizeof(char) * 27); // 0 para o numero de atributos e outras 26 para caber o alfabeto inteiro
+
+    //Fazer laço pegando cada atributo e colocando no vetor
+    //E colocar o tamanho no atributos[0]
+
+    return atributos;
+}
 
 /* Só para testar */
 int main() {
     
-    FILE *arq = devolveArquivo("./exemplos/1.fds"); // Será que precisamos verificar se tem .fds ?
-    
-    
+    FILE *arq = devolveArquivo("./exemplos/1.fds"); 
+    char *atributos = devolveArquivo(arq);
+
+    free(atributos);
     fclose(arq);
     printf("Arquivo aberto\n");
     return 0;
