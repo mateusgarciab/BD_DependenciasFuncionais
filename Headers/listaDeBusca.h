e7bcfb14ace1f7a261c707f1d0ce39bb5ca87df6
Mateus Garcia <101533021+mateusgarciab@users.noreply.github.com>
2025-11-27 02:03:57 +0000

    comentario

--- Headers/listaDeBusca.h
+++ Headers/listaDeBusca.h
@@ -9,21 +9,28 @@ struct lista {
     char **atributos;
 };
 
-
+//Cria uma lista e devolve o ponteiro para ela
 struct lista* criaLista(char *atr);
 
+//Utilizada para o uso do qsort
 int comparaAtributos(const void *a, const void *b);
 
+//Insere um atributo na lista
 void insereLista(struct lista *l, char *atr);
 
+//Verifica se a lista esta vazia
 unsigned char listaVazia(struct lista *l);
 
+//Devolve um item da lista
 char *devolveItemLista(struct lista *l);
 
+//Libera lista da memória
 void liberaLista(struct lista *l);
 
+//Verifica se um atributo ou conjunto de atributos é chave
 int ehChave(struct listaDependencias *lista, char *item, char *atr);
 
+//Verifica se um atributo faz parte de uma chave
 int fazParteChave(char letra, char *item);
 
 
--- Headers/operacoes.h
+++ Headers/operacoes.h
@@ -16,14 +16,31 @@ struct chaves {
     int qtd;
 };
 
+//Cria uma lista de dependencias separando as que estão do lado esquerdo e direito do ->
 struct listaDependencias* devolveDependencia(char* dependencias);
 
+//A partir da lista de dependências calcula o fecho de um atributo X
 char *calculaFecho(struct listaDependencias *lista, char *X);
 
+//A partir da lista de dependências calcula as três fases da cobertura mínima
 void calcularCoberturaMinima(struct Dependencia *DF, int qtd);
 
+//Gera uma lista de chaves candidatas a partir da lista de dependências e os atributos
 struct chaves *buscaLargura(struct listaDependencias *lista, char* atr);
 
+//Verifica se um atributo já está na lista de primos, usada para formasNormais
+int jaEhprimo(char *primos, char atr, int tamPrimos);
+
+//Verifica se o lado esquerdo está nas chaves, usada para formasNormais
+int estaNasChaves(struct chaves *c, char *ladoEsquerdo);
+
+//Verifica se as dependências estão em BCNF
+char *ehBCNF(struct listaDependencias *l, struct chaves *c);
+
+//Verifica se as dependências estão em 3FN
+char *eh3FN(struct listaDependencias *l, struct chaves *c, char *primos)
+
+//Verifica as formas normais 3FN e BCNF
 void formasNormais(struct listaDependencias *l, char *atr);
 
 #endif
